6453db358e1dfd6a14995e7bd733fe2c49378734
정형진 <pnescene@icloud.com>
2022-02-08 20:28:48 +0900

    레이아웃 스타일컴포넌트화

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,24 @@
 import React from "react"
 import Navbar from "./NavBar"
+import "../styles/global.css"
+import styled from "styled-components"
+
+const LayoutContainer = styled.div`
+  max-width: 1280px;
+  padding: 0 40px;
+  margin: 0 auto;
+  line-height: 1.4;
+`
 
 function Layout({ children }) {
   return (
-    <div className="layout">
+    <LayoutContainer>
       <Navbar />
       <div className="content">{children}</div>
       <footer>
         <p>Copyright 2022 Hyeongjin Jeong</p>
       </footer>
-    </div>
+    </LayoutContainer>
   )
 }
 
